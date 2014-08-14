11feaea87610277cc2ab1eb8812aa294370c25e8
julisajimenez97@yahoo.com <>
2014-08-14 16:59:59 +0000

    indexnmake box float

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Initialize phaser engine. create a 400x490px game!
 
-var game = new Phaser.Game(400,490, Phaser,AUTO, 'gameDiv');
+var game = new Phaser.Game(400,490, Phaser.AUTO, 'gameDiv');
 
 //Create our 'main' state that will contain the game
 //This is the body of the game itself
@@ -15,26 +15,66 @@ var mainState = {
     //Set the background color of the game
     game.stage.backgroundColor ="#71c5cf";
     
+    game.load.image('bird', 'assets/bird.png');
     
-  },
+    game.load.image('pipe', 'assets/pipe.png');
+    
+    
+     },
   
   
     create: function () {
       //This function is called right after preload function 
       //This is where we set up the game assets from earlier
-  },
+      
+      game.physics.startSystem(Phaser.Physics.Arcade);
+      
+      this.bird = this.game.add.sprite(100, 245, 'bird');
+      
+      
+      //Now that we have a bird and grvity... we need to tell the bird 
+      //to react to gravity
+      
+      game.physics.arcade.enable(this.bird);
+      
+      this.bird.body.gravity.y = 1000;
+      
+      var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+      
+      spaceKey.onDown.add(this.jump, this);
+      },
   
   update: function () {
     //This function runs 60 times per second
+    
+    
+    //check if the  bird is outside the gamescreen
+    if (this.bird.inWorld == false) {
+      this.restartGame();
+       }
+    
     },
     
-    }
+    
+    
+    jump:function () {
+      
+      //Let's make our bird jump!
+      this.bird.body.velocity.y = -350;
+       },
+    
+    restartGame: function (){
+    
+    game.state.start('main');
+    },
+    };
+   
   
   
   //Add and start the 'mainState' to start the game
   game.state.add('main', mainState);
   game.state.start('main');
   
-  
+ 
   
   
